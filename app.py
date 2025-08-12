62e31eb77b55c0724216dcf572242cd4ef70669e
Nidal Jaafar <nidaljaafar.nj@gmail.com>
2025-08-12 03:26:28 +0300

    added image size validation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from flask import Flask, jsonify, request, send_file, render_template
 
@@ -23,21 +24,10 @@ def health():
 @app.route("/api/v1/predict", methods=["POST"])
 def predict():
     log.info("Received request")
-
-    if 'image' not in request.files:
-        log.info("No image provided")
-        return jsonify({'error': 'No image provided'}), 400
-
-    file = request.files['image']
-    if file.filename == '':
-        log.info("No image provided")
-        return jsonify({'error': 'No image provided'}), 400
-
-    if file.mimetype != "image/jpeg":
-        log.info("Invalid mimetype")
-        log.info(f"Received mimetype: {file.mimetype}")
-        return jsonify({'error': 'Invalid image format'}), 400
-
+    res = validate_request()
+    if len(res) == 3:
+        return res[1], res[2]
+    file = res[1]
     try:
         log.info("Starting prediction")
         prediction = model.predict(file)
@@ -51,6 +41,32 @@ def predict():
         mimetype='image/jpeg',
         download_name='predicted_image.jpg'
     )
+
+def validate_request():
+    log.info("Validating request")
+
+    if 'image' not in request.files:
+        log.info("No image provided")
+        return False ,jsonify({'error': 'No image provided'}), 400
+
+    file = request.files['image']
+    if file.filename == '':
+        log.info("No image provided")
+        return False, jsonify({'error': 'No image provided'}), 400
+
+    if file.mimetype != "image/jpeg":
+        log.info("Invalid mimetype")
+        log.info(f"Received mimetype: {file.mimetype}")
+        return False, jsonify({'error': 'Invalid image format'}), 400
+
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    if file_size > 1024 * 1024:
+        log.info(f"Image too large: {file_size} bytes")
+        return False, jsonify({'error': 'Image too large'}), 400
+    return True, file
 
 
 def main():
