884ead450aa39cf12e2b4095bcc1a2019f36d8af
Nidal Jaafar <nidaljaafar.nj@gmail.com>
2025-08-14 23:08:47 +0300

    changed the max image size and added image resizing logic

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import io
 import logging
 import os
 
+from PIL import Image
 from flask import Flask, jsonify, request, send_file, render_template
 
 from model.model import Model
@@ -27,7 +29,7 @@ def predict():
     res = validate_request()
     if len(res) == 3:
         return res[1], res[2]
-    file = res[1]
+    file = resize(res[1])
     try:
         log.info("Starting prediction")
         prediction = model.predict(file)
@@ -47,7 +49,7 @@ def validate_request():
 
     if 'image' not in request.files:
         log.info("No image provided")
-        return False ,jsonify({'error': 'No image provided'}), 400
+        return False, jsonify({'error': 'No image provided'}), 400
 
     file = request.files['image']
     if file.filename == '':
@@ -63,10 +65,49 @@ def validate_request():
     file_size = file.tell()
     file.seek(0)
 
-    if file_size > 1024 * 1024:
+    if file_size > 5 * 1024 * 1024:
         log.info(f"Image too large: {file_size} bytes")
         return False, jsonify({'error': 'Image too large'}), 400
     return True, file
+
+
+def resize(file):
+    log.info("Resizing image")
+    img_bytes = file.read()
+    img = Image.open(io.BytesIO(img_bytes))
+
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+
+    width, height = img.size
+    quality = 95
+
+    while True:
+        temp_img = img.copy()
+
+        if width > 1200 or height > 1200:
+            temp_img.thumbnail((width, height), Image.Resampling.LANCZOS)
+
+        img_io = io.BytesIO()
+        temp_img.save(img_io, format='JPEG', quality=quality, optimize=True)
+
+        img_size = img_io.tell()
+        log.info(f"Image size: {img_size} bytes")
+
+        if img_size <= 1024 * 1024:
+            img_io.seek(0)
+            return img_io
+
+        if quality > 50:
+            quality -= 10
+        else:
+            width = int(width * 0.9)
+            height = int(height * 0.9)
+            quality = 85
+
+            if width < 100 or height < 100:
+                img_io.seek(0)
+                return img_io
 
 
 def main():
